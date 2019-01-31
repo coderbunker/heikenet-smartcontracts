7de1628e1b7cd3a843a223b68dbf3bf78b1fe931
Arseny Akinfiev <arseny.jin@ya.ru>
2019-01-31 12:21:13 +0800

    ADD: set up token issuence functionality

--- heike/contracts/token.sol
+++ heike/contracts/token.sol
@@ -8,11 +8,23 @@ import "./ERC20/MintableToken.sol";
 /**
  * The contractName contract does this and that...
  */
-contract Token is ERC20, MintableToken {
+contract IssueToken is ERC20, MintableToken {
 
-	function myFunction () public returns(bool) {
-		return true;
-	}
-	
+    string public symbol;
+    string public  name;
+    uint8 public decimals;
+    uint public _totalSupply;
+
+    mapping(address => uint) balances;
+    mapping(address => mapping(address => uint)) allowed;
+
+    constructor() public {
+        symbol = "HUSD";
+        name = "Heike USD";
+        decimals = 18;
+        _totalSupply = 0;
+        balances[0x5A86f0cafD4ef3ba4f0344C138afcC84bd1ED222] = _totalSupply; // Adress should be changed for an entity adress
+        emit Transfer(address(0), 0x5A86f0cafD4ef3ba4f0344C138afcC84bd1ED222, _totalSupply); // Adress should be changed to an entity adress
+    }	
 
 }
